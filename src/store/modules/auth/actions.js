29259f307b981002e7e6978c8ede5e0651ab35c5
viitor14 <manoelvitor550@gmail.com>
2025-09-16 22:19:23 -0300

    criação das actions de REGISTER

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -19,3 +19,23 @@ export function loginFailure(payload) {
     type: types.LOGIN_FAILURE
   };
 }
+
+export function registerRequest(payload) {
+  return {
+    type: types.REGISTER_REQUEST,
+    payload
+  };
+}
+
+export function registerSuccess(payload) {
+  return {
+    type: types.REGISTER_SUCCESS,
+    payload
+  };
+}
+
+export function registerFailure(payload) {
+  return {
+    type: types.REGISTER_FAILURE
+  };
+}
